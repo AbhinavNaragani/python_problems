1d3537a06dba30700e042bb68f3093e4c65743b0
Abhinav Chowdary <abhinavchowdary@Abhinavs-Air.lan>
2024-02-12 20:35:02 -0500

    reverse pattern

---
+++ week 1/patterns.py
@@ -0,0 +1,17 @@
+# '''
+# 3.  *****
+#     ****
+#     ***
+#     **
+#     *
+# '''
+
+i=1
+n=int(input("enter n"))
+while i<=n:
+    j=1
+    while j<=(n-i+1):
+        print("*", end="")
+        j=j+1
+    print()
+    i=i+1
